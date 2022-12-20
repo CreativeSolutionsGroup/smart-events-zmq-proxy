dc3d8406ab261a65bf709628278fd5bab0af06ba
SpBills <spencebills0@gmail.com>
2022-12-20 15:54:25 -0500

    Added binds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use zmq::proxy;
+
 fn main() {
-    println!("Hello, world!");
+    let context = zmq::Context::new();
+    let xpub = context.socket(zmq::XPUB).unwrap();
+    xpub.bind("tcp://*:9951").unwrap();
+    let xsub = context.socket(zmq::XSUB).unwrap();
+    xsub.bind("tcp://*:9950").unwrap();
+
+    println!("Running the proxy. 🚀🚀🚀");
+    proxy(&xpub, &xsub).expect("Failed to create proxy.");
 }
