2ff6a9556b586ac34a48ed28c336685b1cbb3387
Sebastian Neiswanger <sebastianneiswanger@gmail.com>
2023-01-15 18:37:22 -0500

    added capture onto proxy

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use zmq::proxy;
+use zmq::proxy_with_capture;
 
 fn main() {
     let context = zmq::Context::new();
-    let xpub = context.socket(zmq::ROUTER).unwrap();
-    xpub.bind("tcp://*:9951").unwrap();
-    let xsub = context.socket(zmq::DEALER).unwrap();
-    xsub.bind("tcp://*:9950").unwrap();
-
+    let mut router = context.socket(zmq::ROUTER).unwrap();
+    router.bind("tcp://*:9951").unwrap();
+    let mut dealer = context.socket(zmq::DEALER).unwrap();
+    dealer.bind("tcp://*:9950").unwrap();
+    let mut push = context.socket(zmq::PAIR).unwrap();
+    push.bind("tcp://*:9952").unwrap();
+    
     println!("Running the proxy. 🚀🚀🚀");
-    proxy(&xpub, &xsub).expect("Failed to create proxy.");
+    proxy_with_capture(&mut router, &mut dealer, &mut push).expect("Failed to create proxy.");
 }
