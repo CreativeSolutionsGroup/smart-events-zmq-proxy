a0eb6f1bb0725b937b38cfbf5f8084f0a026d56f
spbills <spencebills0@gmail.com>
2023-01-30 15:56:13 -0500

    Changed to pub

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ fn main() {
     router.bind("tcp://*:9951").unwrap();
     let mut dealer = context.socket(zmq::DEALER).unwrap();
     dealer.bind("tcp://*:9950").unwrap();
-    let mut push = context.socket(zmq::PAIR).unwrap();
-    push.bind("tcp://*:9952").unwrap();
+    let mut publish = context.socket(zmq::PUB).unwrap();
+    publish.bind("tcp://*:9960").unwrap();
     
     println!("Running the proxy. 🚀🚀🚀");
-    proxy_with_capture(&mut router, &mut dealer, &mut push).expect("Failed to create proxy.");
+    proxy_with_capture(&mut router, &mut dealer, &mut publish).expect("Failed to create proxy.");
 }
